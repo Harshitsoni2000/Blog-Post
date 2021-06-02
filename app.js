894907ee9dbd8f76a9e62508a034f8397481bbc4
Harshit Soni <harshitsoni.ajmer23@gmail.com>
2021-06-03 02:03:47 +0530

    Final Challenge-Add Databse

--- app.js
+++ app.js
@@ -4,6 +4,26 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose=require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/postsDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+//Mongoose & Mongodb
+
+const postSchema= {
+  title: {
+    type: String,
+    required: [1, "Please check Entered value again, and enter a title"]
+  },
+  body: {
+    type: String,
+    required: [1, "Please check the Entered value again, and Enter a post Message"]
+  }
+};
+
+const Post=mongoose.model("post", postSchema);
+
+//-------------
 
 const app = express();
 
@@ -16,7 +36,6 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutStartingContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactStartingContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-const posts=[];
 let any="";
 
 app.listen(3000, function() {
@@ -24,7 +43,9 @@ app.listen(3000, function() {
 });
 
 app.get("/", (req, res)=> {
-  res.render("home", {homeContent: homeStartingContent, posts: posts});
+  Post.find((err, docs)=> {
+    res.render("home", {homeContent: homeStartingContent, posts: docs});
+  });
 });
 
 app.get("/about", (req, res)=> {
@@ -40,17 +61,26 @@ app.get("/compose", (req, res)=> {
 });
 
 app.get("/posts/:Any", (req, res)=> {
-  posts.forEach((post)=> {
-    if(_.lowerCase(post.title)==_.lowerCase(req.params.Any))
-      res.render("post", {title: post.title, body: post.body});
+  Post.find((err, docs)=> {
+    docs.forEach((doc)=> {
+      if(_.lowerCase(doc.title)==_.lowerCase(req.params.Any)) {
+        res.render("post", {title: doc.title, body: doc.body});
+      }
+    });
+  });
+});
+
+app.post("/delete", (req, res)=> {
+  Post.findOneAndDelete({title: req.body.checkbox}, (err, doc)=> {
+    res.redirect("/");
   });
 });
 
 app.post("/compose", (req, res)=> {
-  const post={
+  const post=new Post({
     title: req.body.postTitle,
     body: req.body.postBody
-  };
-  posts.push(post);
+  });
+  post.save();
   res.redirect("/");
 });
